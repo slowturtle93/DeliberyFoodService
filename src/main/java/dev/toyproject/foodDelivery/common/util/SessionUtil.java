3294f660abe29b0a97786dfe0b4b56d3aff777b2
haksong <gkrthdz@naver.com>
2021-11-18 22:25:46 +0900

    feat: Owner Session 기능 추가

--- src/main/java/dev/toyproject/foodDelivery/common/util/SessionUtil.java
+++ src/main/java/dev/toyproject/foodDelivery/common/util/SessionUtil.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpSession;
 public class SessionUtil {
 
     private static final String LOGIN_MEMBER_ID = "LOGIN_MEMBER_ID";
-    private static final String LOGIN_RIDER_ID = "LOGIN_RIDER_ID";
+    private static final String LOGIN_OWNER_TOKEN  = "LOGIN_OWNER_TOKEN";
 
     // 인스턴스화 방지
     private SessionUtil(){}
@@ -39,5 +39,36 @@ public class SessionUtil {
      */
     public static void removeLogoutMember(HttpSession session){
         session.removeAttribute(LOGIN_MEMBER_ID);
+    }
+
+    /********************************* OWNER ***********************************/
+
+    /**
+     * 사장님 session 정보 조회
+     *
+     * @param session
+     * @return
+     */
+    public static String getLoginOwnerToken(HttpSession session) {
+        return (String) session.getAttribute(LOGIN_OWNER_TOKEN);
+    }
+
+    /**
+     * 사장님 Token 정보 session 저장
+     *
+     * @param session
+     * @param ownerToken
+     */
+    public static void setLoginOwnerToken(HttpSession session, String ownerToken){
+        session.setAttribute(LOGIN_OWNER_TOKEN, ownerToken);
+    }
+
+    /**
+     * 사장님 Token 정보 session 삭제
+     *
+     * @param session
+     */
+    public static void removeLogoutOwner(HttpSession session){
+        session.removeAttribute(LOGIN_OWNER_TOKEN);
     }
 }
