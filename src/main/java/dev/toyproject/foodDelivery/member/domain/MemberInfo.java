6cb6a309e15dfec0af1aeca3e8808ed036016553
haksong <gkrthdz@naver.com>
2021-12-01 21:31:55 +0900

    feat: MemberInfo 구조 변경

--- src/main/java/dev/toyproject/foodDelivery/member/domain/MemberInfo.java
+++ src/main/java/dev/toyproject/foodDelivery/member/domain/MemberInfo.java
@@ -2,29 +2,32 @@ package dev.toyproject.foodDelivery.member.domain;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 /**
  * INFO - 사용자 정보 Select 시 사용되는 객체
  */
-@Getter
 public class MemberInfo {
 
-    private String memberMail;           // 사용자 이메일
-    private String memberToken;          // 사용자 Token 정보
-    private String memberPwd;            // 사용자 비밀번호
-    private String memberTel;            // 사용자 전화번호
-    private String memberNickname;       // 사용자 닉네임
-    private String addressCode;          // 사용자 주소코드
-    private Member.Status status;        // 사용자 상태
+    @Getter
+    @ToString
+    public static class Main{
+        private String memberMail;           // 사용자 이메일
+        private String memberToken;          // 사용자 Token 정보
+        private String memberPwd;            // 사용자 비밀번호
+        private String memberTel;            // 사용자 전화번호
+        private String memberNickname;       // 사용자 닉네임
+        private String addressCode;          // 사용자 주소코드
+        private Member.Status status;        // 사용자 상태
 
-    @Builder
-    public MemberInfo(Member member){
-        this.memberMail     = member.getMemberMail();
-        this.memberToken    = member.getMemberToken();
-        this.memberPwd      = member.getMemberPwd();
-        this.memberTel      = member.getMemberTel();
-        this.memberNickname = member.getMemberNickname();
-        this.addressCode    = member.getAddressCode();
-        this.status         = member.getStatus();
+        public Main(Member member){
+            this.memberMail     = member.getMemberMail();
+            this.memberToken    = member.getMemberToken();
+            this.memberPwd      = member.getMemberPwd();
+            this.memberTel      = member.getMemberTel();
+            this.memberNickname = member.getMemberNickname();
+            this.addressCode    = member.getAddressCode();
+            this.status         = member.getStatus();
+        }
     }
 }
