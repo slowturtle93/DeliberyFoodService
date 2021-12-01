0b58e7c08213dedc07430b9a2fd220672074bdd2
haksong <gkrthdz@naver.com>
2021-12-01 21:34:08 +0900

    feat: 본인인증 기능 추가 및 Command, Info -> .Main으로 변경

--- src/main/java/dev/toyproject/foodDelivery/member/application/MemberFacade.java
+++ src/main/java/dev/toyproject/foodDelivery/member/application/MemberFacade.java
@@ -23,7 +23,7 @@ public class MemberFacade {
      * @param command
      * @return
      */
-    public MemberInfo registerMember(MemberCommand command){
+    public MemberInfo.Main registerMember(MemberCommand.Main command){
         var memberInfo = memberService.registerMember(command); // 사용자 등록
         return memberInfo;
     }
@@ -35,7 +35,7 @@ public class MemberFacade {
      * @param session
      * @return
      */
-    public MemberInfo loginMember(MemberCommand command, HttpSession session){
+    public MemberInfo.Main loginMember(MemberCommand.Main command, HttpSession session){
         var loginMemberInfo = memberService.loginMemberInfo(command.getMemberMail(), command.getMemberPwd()); // 로그인 정보 확인
         SessionUtil.setLoginMemberToken(session, loginMemberInfo.getMemberToken()); // session 에 사용자 Token 정보 저장
         return loginMemberInfo;
@@ -65,7 +65,7 @@ public class MemberFacade {
      * @param command
      * @return
      */
-    public MemberInfo updateMember(MemberCommand command){
+    public MemberInfo.Main updateMember(MemberCommand.Main command){
         var memberInfo = memberService.updateMember(command); // 사용자 정보 수정
         return memberInfo;
     }
@@ -76,7 +76,7 @@ public class MemberFacade {
      * @param command
      * @return
      */
-    public MemberInfo updateMemberPassword(MemberCommand command, String afterPassword, HttpSession session){
+    public MemberInfo.Main updateMemberPassword(MemberCommand.Main command, String afterPassword, HttpSession session){
         var MemberInfo = memberService.updateMemberPassword(command, afterPassword); // 비밀번호 변경
         SessionUtil.removeLogoutMember(session); // 비밀번호 변경 후 재접속을 위해 session 값 삭제
         return MemberInfo;
@@ -91,5 +91,13 @@ public class MemberFacade {
     public void disableMember(String memberToken, HttpSession session){
         memberService.disableMember(memberToken); // 사용자 상태 [DISABLE] 변경
         SessionUtil.removeLogoutMember(session);  // 회원 탈퇴 시 Session 정보 삭제
+    }
+
+    /**
+     * 본인인증 확인
+     */
+    public MemberInfo.Main authCheck(MemberCommand.Main command){
+        var memberInfo = memberService.authCheck(command);
+        return memberInfo;
     }
 }
