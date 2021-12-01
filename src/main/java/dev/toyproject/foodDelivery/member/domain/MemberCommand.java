45f0555cc36cbc833494fe607e0c434336706242
haksong <gkrthdz@naver.com>
2021-12-01 21:31:40 +0900

    feat: MemberCommand 구조 변경

--- src/main/java/dev/toyproject/foodDelivery/member/domain/MemberCommand.java
+++ src/main/java/dev/toyproject/foodDelivery/member/domain/MemberCommand.java
@@ -7,24 +7,26 @@ import lombok.ToString;
 /**
  *  Command - Create, Update, Delete 시 사용되는 객체
  */
-@Getter
-@Builder
-@ToString
 public class MemberCommand {
 
-    private String memberMail;     // 사용자 이메일
-    private String memberToken;    // 사용자 Token
-    private String memberPwd;      // 사용자 비밀번호
-    private String memberTel;      // 사용자 전화번호
-    private String memberNickname; // 사용자 닉네임
+    @Getter
+    @Builder
+    @ToString
+    public static class Main{
+        private String memberMail;     // 사용자 이메일
+        private String memberToken;    // 사용자 Token
+        private String memberPwd;      // 사용자 비밀번호
+        private String memberTel;      // 사용자 전화번호
+        private String memberNickname; // 사용자 닉네임
 
-    public Member toEntity() {
-        return Member.builder()
-                .memberMail(memberMail)
-                .memberToken(memberToken)
-                .memberPwd(memberPwd)
-                .memberTel(memberTel)
-                .memberNickname(memberNickname)
-                .build();
+        public Member toEntity() {
+            return Member.builder()
+                    .memberMail(memberMail)
+                    .memberToken(memberToken)
+                    .memberPwd(memberPwd)
+                    .memberTel(memberTel)
+                    .memberNickname(memberNickname)
+                    .build();
+        }
     }
 }
