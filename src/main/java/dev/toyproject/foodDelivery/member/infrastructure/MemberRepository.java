7d43ceeee6c68c28e6a537444b03184ba998d5cc
haksong <gkrthdz@naver.com>
2021-12-01 21:35:45 +0900

    feat: 이메일, 휴대폰 번호로 회원 조회 기능 추가

--- src/main/java/dev/toyproject/foodDelivery/member/infrastructure/MemberRepository.java
+++ src/main/java/dev/toyproject/foodDelivery/member/infrastructure/MemberRepository.java
@@ -14,4 +14,6 @@ public interface MemberRepository extends JpaRepository<Member, String> {
     Optional<Member> findByMemberTokenAndStatus(String memberToken, Member.Status status);
 
     Optional<Member> findByMemberTokenAndMemberPwdAndStatus(String memberToken, String memberPwd, Member.Status status);
+
+    Optional<Member> findByMemberMailAndMemberTelAndStatus(String memberMail, String memberTel, Member.Status status);
 }
