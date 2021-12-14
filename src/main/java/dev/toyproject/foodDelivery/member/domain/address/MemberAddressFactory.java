502833a9f16957fc6a2903dd79b1273ce73ffd7d
haksong <gkrthdz@naver.com>
2021-12-14 22:33:26 +0900

    feat: 사용자 배달 저장 시 Status 수정 기능 구현

---
+++ src/main/java/dev/toyproject/foodDelivery/member/domain/address/MemberAddressFactory.java
@@ -0,0 +1,7 @@
+package dev.toyproject.foodDelivery.member.domain.address;
+
+import java.util.List;
+
+public interface MemberAddressFactory {
+    void memberAddressListDisable(List<MemberAddress> memberAddressList);
+}
