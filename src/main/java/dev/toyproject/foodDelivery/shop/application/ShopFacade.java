d6902f3ef61ed6a9112fea92d615db973f525bae
haksong <gkrthdz@naver.com>
2021-12-15 20:45:57 +0900

    feat: 사장님 가게 application 기능 구현

---
+++ src/main/java/dev/toyproject/foodDelivery/shop/application/ShopFacade.java
@@ -0,0 +1,26 @@
+package dev.toyproject.foodDelivery.shop.application;
+
+import dev.toyproject.foodDelivery.shop.domain.ShopCommand;
+import dev.toyproject.foodDelivery.shop.domain.ShopService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ShopFacade {
+
+    private final ShopService shopService;
+
+    /**
+     * 사장님 가게 등록
+     *
+     * @param ShopCommand
+     * @return
+     */
+    public String registerShop(ShopCommand.ShopRequest ShopCommand){
+        var shopToken = shopService.registerShop(ShopCommand); // 가게 등록
+        return shopToken;
+    }
+}
