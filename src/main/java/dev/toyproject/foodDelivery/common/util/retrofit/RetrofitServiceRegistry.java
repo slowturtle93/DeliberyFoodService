31e3585478b2b7d864993ed6199b6caac8633914
haksong <gkrthdz@naver.com>
2021-12-15 20:42:31 +0900

    feat: kakao 주소 API Retrofit 추가

--- src/main/java/dev/toyproject/foodDelivery/common/util/retrofit/RetrofitServiceRegistry.java
+++ src/main/java/dev/toyproject/foodDelivery/common/util/retrofit/RetrofitServiceRegistry.java
@@ -1,5 +1,6 @@
 package dev.toyproject.foodDelivery.common.util.retrofit;
 
+import dev.toyproject.foodDelivery.address.infrastructure.RetrofitKakaoAddressApi;
 import dev.toyproject.foodDelivery.notification.sms.infrastructure.RetrofitNaverSensApi;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,11 +14,22 @@ public class RetrofitServiceRegistry {
     // Naver SENS API
     @Value("${spring.sens-api.url}")
     private String NaverSmsApi;
+
+    // API Host 설정
+    @Value("${spring.address-api.url}")
+    private String kakaoAddressApi;
     
     // Retrofit Bean 생성
     @Bean
     public RetrofitNaverSensApi retrofitNaverSensApi(){
         var retrofit = RetrofitUtils.initRetrofit(NaverSmsApi);
         return retrofit.create(RetrofitNaverSensApi.class);
+    }
+
+    // Retrofit Bean 생성
+    @Bean
+    public RetrofitKakaoAddressApi retrofitKakaoAddressApi(){
+        var retrofit = RetrofitUtils.initRetrofit(kakaoAddressApi);
+        return retrofit.create(RetrofitKakaoAddressApi.class);
     }
 }
