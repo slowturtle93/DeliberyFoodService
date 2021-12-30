e038c9745096f4bdba971ddd4df222f5acec8c4f
haksong <gkrthdz@naver.com>
2021-12-30 22:52:53 +0900

    feat: 가게 정보 조회 로직 변경 및 특정 기반 위치 반경 이내 가게 조회 기능 추가

--- src/main/java/dev/toyproject/foodDelivery/shop/domain/ShopReader.java
+++ src/main/java/dev/toyproject/foodDelivery/shop/domain/ShopReader.java
@@ -1,8 +1,14 @@
 package dev.toyproject.foodDelivery.shop.domain;
 
+import java.util.List;
+
 public interface ShopReader {
 
     public Shop getShopByToken(String shopToken);
 
     public ShopInfo.Main getShopSeries(Shop shop);
+
+    public List<Shop> searchShop(ShopCommand.MemberLocationRequest request);
+
+    public List<ShopInfo.ShopMain> getShopListSeries(List<Shop> shopList);
 }
--- src/main/java/dev/toyproject/foodDelivery/shop/infrastructure/ShopReadImpl.java
+++ src/main/java/dev/toyproject/foodDelivery/shop/infrastructure/ShopReadImpl.java
@@ -2,11 +2,15 @@ package dev.toyproject.foodDelivery.shop.infrastructure;
 
 import dev.toyproject.foodDelivery.common.exception.EntityNotFoundException;
 import dev.toyproject.foodDelivery.shop.domain.Shop;
+import dev.toyproject.foodDelivery.shop.domain.ShopCommand;
 import dev.toyproject.foodDelivery.shop.domain.ShopInfo;
 import dev.toyproject.foodDelivery.shop.domain.ShopReader;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -14,6 +18,7 @@ import org.springframework.stereotype.Component;
 public class ShopReadImpl implements ShopReader {
 
     private final ShopRepository shopRepository;
+    private final ShopRepositorySupportImpl shopRepositorySupport;
 
     /**
      * 특정 가게 조회 By shopToken
@@ -38,6 +43,63 @@ public class ShopReadImpl implements ShopReader {
         var shopBusinessInfo = new ShopInfo.ShopBusinessInfo(shop.getShopBusiness());
         var shopAddressInfo = new ShopInfo.ShopAddressInfo(shop.getShopAddress());
 
-        return new ShopInfo.Main(shop, shopBusinessInfo, shopAddressInfo);
+        var shopMenuGroupList = shop.getMenuGroupList();
+        var menuGroupInfoList = shopMenuGroupList.stream()
+                .map(shopMenuGroup -> {
+
+                    // menuInfo list
+                    var menuList = shopMenuGroup.getMenuList();
+                    var menuInfoList = menuList.stream()
+                            .map(menu -> {
+
+                                // menu Option Group List
+                                var menuOptionGroupList = menu.getMenuOptionGroupList();
+                                var menuOptionGroupInfoList = menuOptionGroupList.stream()
+                                        .map(menuOptionGroup ->{
+
+                                            // menu Option List
+                                            var menuOptionList = menuOptionGroup.getMenuOptionList();
+                                            var menuOptionInfoList = menuOptionList.stream()
+                                                    .map(ShopInfo.MenuOptionInfo::new)
+                                                    .collect(Collectors.toList());
+
+                                            return new ShopInfo.MenuOptionGroupInfo(menuOptionGroup, menuOptionInfoList);
+                                        }).collect(Collectors.toList());
+
+                                return new ShopInfo.MenuInfo(menu, menuOptionGroupInfoList);
+                            }).collect(Collectors.toList());
+
+                    return new ShopInfo.MenuGroupInfo(shopMenuGroup, menuInfoList);
+                }).collect(Collectors.toList());
+
+        return new ShopInfo.Main(shop, shopBusinessInfo, shopAddressInfo, menuGroupInfoList);
+    }
+
+    /**
+     * nativeQuery 를 통해 특정 좌표 위치 기반으로 반경 2km 이내의 가게 조회
+     *
+     * @param request
+     * @return
+     */
+    @Override
+    public List<Shop> searchShop(ShopCommand.MemberLocationRequest request) {
+        var shopList = shopRepositorySupport.findSearchShop(request);
+        return shopList;
+    }
+
+    /**
+     * 가게 정보 List Series
+     *
+     * @param shopList
+     * @return
+     */
+    @Override
+    public List<ShopInfo.ShopMain> getShopListSeries(List<Shop> shopList) {
+
+        var shopInfoList = shopList;
+
+        return shopInfoList.stream()
+                .map(ShopInfo.ShopMain::new)
+                .collect(Collectors.toList());
     }
 }
