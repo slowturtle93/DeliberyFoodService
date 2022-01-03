f70421ca3f995168e097660abbc09c557d1201b2
haksong <gkrthdz@naver.com>
2022-01-03 18:00:53 +0900

    feat: OrderMenu Entity 생성

---
+++ src/main/java/dev/toyproject/foodDelivery/order/domain/menu/OrderMenu.java
@@ -0,0 +1,55 @@
+package dev.toyproject.foodDelivery.order.domain.menu;
+
+import dev.toyproject.foodDelivery.AbstracEntity;
+import dev.toyproject.foodDelivery.common.exception.InvalidParamException;
+import dev.toyproject.foodDelivery.order.domain.Order;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.persistence.*;
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "order_menu")
+public class OrderMenu extends AbstracEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String orderMenuName;
+    private Long orderMenuCount;
+    private Long orderMenuPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "orderMenu", cascade = CascadeType.PERSIST)
+    private List<OrderMenuOption> orderMenuOptionList = Arrays.asList();
+
+    @Builder
+    public OrderMenu(
+            String orderMenuName,
+            Long orderMenuCount,
+            Long orderMenuPrice,
+            Order order
+    ){
+        if (StringUtils.isEmpty(orderMenuName)) throw new InvalidParamException("orderMenu.orderMenuName");
+        if (orderMenuCount == null)             throw new InvalidParamException("orderMenu.orderMenuCount");
+        if (orderMenuPrice == null)             throw new InvalidParamException("orderMenu.orderMenuPrice");
+        if (order == null)                      throw new InvalidParamException("orderMenu.order");
+
+        this.orderMenuName  = orderMenuName;
+        this.orderMenuCount = orderMenuCount;
+        this.orderMenuPrice = orderMenuPrice;
+        this.order          = order;
+    }
+}
