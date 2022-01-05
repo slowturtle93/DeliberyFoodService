adaec50ce14a3386280186755f1dbad6ffd54706
haksong <gkrthdz@naver.com>
2022-01-05 22:37:38 +0900

    feat: 메뉴 장바구니 등록 기능 추가

--- src/main/java/dev/toyproject/foodDelivery/order/interfaces/OrderDto.java
+++ src/main/java/dev/toyproject/foodDelivery/order/interfaces/OrderDto.java
@@ -1,4 +1,41 @@
 package dev.toyproject.foodDelivery.order.interfaces;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
 public class OrderDto {
+
+    /******************************** request ********************************/
+
+    @Getter
+    @Setter
+    @ToString
+    public static class OrderBasketRequest{
+        private String memberToken;
+        private String shopToken;
+        private OrderBasketMenuRequest orderBasketMenu;
+    }
+
+    @Getter
+    @Setter
+    @ToString
+    public static class OrderBasketMenuRequest{
+        private Long id;
+        private String orderMenuName;
+        private Long orderMenuCount;
+        private Long orderMenuPrice;
+        private List<OrderBasketMenuOptionRequest> orderBasketMenuOptionList;
+    }
+
+    @Getter
+    @Setter
+    @ToString
+    public static class OrderBasketMenuOptionRequest{
+        private Long id;
+        private String orderMenuOptionName;
+        private Long orderMenuOptionPrice;
+    }
 }
