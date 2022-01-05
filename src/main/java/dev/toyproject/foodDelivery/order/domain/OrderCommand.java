79c3c79914526977298f2356611598e50072df02
haksong <gkrthdz@naver.com>
2022-01-05 22:42:00 +0900

    feat: Menu 장바구니 Request 추가

--- src/main/java/dev/toyproject/foodDelivery/order/domain/OrderCommand.java
+++ src/main/java/dev/toyproject/foodDelivery/order/domain/OrderCommand.java
@@ -1,4 +1,43 @@
 package dev.toyproject.foodDelivery.order.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
 public class OrderCommand {
+
+    @Getter
+    @Setter
+    @Builder
+    @ToString
+    public static class OrderBasketRequest{
+        private String memberToken;
+        private String shopToken;
+        private OrderBasketMenuRequest orderBasketMenu;
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    @ToString
+    public static class OrderBasketMenuRequest{
+        private Long id;
+        private String orderMenuName;
+        private Long orderMenuCount;
+        private Long orderMenuPrice;
+        private List<OrderBasketMenuOptionRequest> orderBasketMenuOptionList;
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    @ToString
+    public static class OrderBasketMenuOptionRequest{
+        private Long id;
+        private String orderMenuOptionName;
+        private Long orderMenuOptionPrice;
+    }
 }
