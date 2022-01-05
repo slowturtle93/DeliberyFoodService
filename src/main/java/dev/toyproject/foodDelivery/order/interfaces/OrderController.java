7b20c544a4b3b7438e7466d2e985b0d7cbab2be7
haksong <gkrthdz@naver.com>
2022-01-05 22:38:07 +0900

    feat: Menu 장바구니 등록 기능 구현

--- src/main/java/dev/toyproject/foodDelivery/order/interfaces/OrderController.java
+++ src/main/java/dev/toyproject/foodDelivery/order/interfaces/OrderController.java
@@ -1,13 +1,35 @@
 package dev.toyproject.foodDelivery.order.interfaces;
 
+import dev.toyproject.foodDelivery.common.response.CommonResponse;
+import dev.toyproject.foodDelivery.order.application.OrderFacade;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/order")
 public class OrderController {
+
+    private final OrderFacade orderFacade;
+    private final OrderDtoMapper orderDtoMapper;
+
+    /**
+     * 장바구니 메뉴 등록
+     *
+     * @param request
+     * @return
+     */
+    @PostMapping("/basket")
+    public CommonResponse registerMenuBasket(@RequestBody @Valid OrderDto.OrderBasketRequest request){
+        var command = orderDtoMapper.of(request);
+        orderFacade.registerMenuBasket(command);
+        return CommonResponse.success("OK");
+    }
 }
