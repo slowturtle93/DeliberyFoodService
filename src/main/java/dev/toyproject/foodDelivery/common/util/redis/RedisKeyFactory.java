95b09fa745b05edf4ccbac8ab0579b74bf33e22b
haksong <gkrthdz@naver.com>
2022-01-05 22:40:07 +0900

    feat: 메뉴 장바구니 Redis Key 추가

--- src/main/java/dev/toyproject/foodDelivery/common/util/redis/RedisKeyFactory.java
+++ src/main/java/dev/toyproject/foodDelivery/common/util/redis/RedisKeyFactory.java
@@ -2,7 +2,11 @@ package dev.toyproject.foodDelivery.common.util.redis;
 
 public class RedisKeyFactory {
 
+    // 사용자 인증번호
     public static final String MEMBER_AUTH_NUMBER = "MEMBER_AUTH_NUMBER";
+
+    // 메뉴 장바구니
+    public static final String MENU_SHOPPING_BASKET = "MENU_SHOPPING_BASKET";
 
     private RedisKeyFactory() {}
 
