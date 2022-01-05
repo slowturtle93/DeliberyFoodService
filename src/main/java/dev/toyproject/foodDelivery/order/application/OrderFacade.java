635f192990c09893e8ab7f7bfde3e28e3b08f825
haksong <gkrthdz@naver.com>
2022-01-05 22:41:32 +0900

    feat: 메뉴 장바구니 기능 메서드 구현

--- src/main/java/dev/toyproject/foodDelivery/order/application/OrderFacade.java
+++ src/main/java/dev/toyproject/foodDelivery/order/application/OrderFacade.java
@@ -1,5 +1,10 @@
 package dev.toyproject.foodDelivery.order.application;
 
+import dev.toyproject.foodDelivery.common.util.redis.RedisCacheUtil;
+import dev.toyproject.foodDelivery.common.util.redis.RedisKeyFactory;
+import dev.toyproject.foodDelivery.order.domain.OrderCommand;
+import dev.toyproject.foodDelivery.order.domain.OrderFactory;
+import dev.toyproject.foodDelivery.order.domain.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -8,4 +13,17 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class OrderFacade {
+
+    private final OrderService orderService;
+    private final OrderFactory orderFactory;
+    private final RedisCacheUtil redisCacheUtil;
+
+    /**
+     * 장바구니 메뉴 등록
+     *
+     * @param command
+     */
+    public void registerMenuBasket(OrderCommand.OrderBasketRequest command){
+        
+    }
 }
