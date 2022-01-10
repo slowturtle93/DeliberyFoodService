47c85b3e6f456376f8583427234c3a406f8cc4f0
haksong <gkrthdz@naver.com>
2022-01-10 22:22:58 +0900

    feat: Redis 장바구니 등록 Service 기능 구현

--- src/main/java/dev/toyproject/foodDelivery/order/domain/OrderService.java
+++ src/main/java/dev/toyproject/foodDelivery/order/domain/OrderService.java
@@ -1,4 +1,8 @@
 package dev.toyproject.foodDelivery.order.domain;
 
+import java.util.List;
+
 public interface OrderService {
+
+    public List<OrderCommand.OrderBasketRequest> registerMenuBasket(OrderCommand.OrderBasketRequest command);
 }
--- src/main/java/dev/toyproject/foodDelivery/order/domain/OrderServiceImpl.java
+++ src/main/java/dev/toyproject/foodDelivery/order/domain/OrderServiceImpl.java
@@ -1,11 +1,33 @@
 package dev.toyproject.foodDelivery.order.domain;
 
+import dev.toyproject.foodDelivery.common.util.redis.RedisCacheUtil;
+import dev.toyproject.foodDelivery.common.util.redis.RedisKeyFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
+
+    private final RedisCacheUtil redisCacheUtil;
+    private final OrderFactory orderFactory;
+
+    /**
+     * 장바구니 메뉴 등록
+     *
+     * @param command
+     */
+    @Override
+    public List<OrderCommand.OrderBasketRequest> registerMenuBasket(OrderCommand.OrderBasketRequest command) {
+        String hashKey = orderFactory.makeHashKey(command);
+        orderFactory.orderBasketShopCheck(command);                                                                               // 같은 가게 확인
+        orderFactory.duplicationMenu(command,hashKey);                                                                            // 메뉴 & 옵션 동일 여부 확인
+        redisCacheUtil.setRedisCacheMenuBasket(command.getMemberToken(), RedisKeyFactory.MENU_SHOPPING_BASKET, hashKey, command); // Redis 등록
+        return redisCacheUtil.getMenuBasketList(command.getMemberToken(), RedisKeyFactory.MENU_SHOPPING_BASKET);                  // 등록된 장바구니 메뉴 조회
+    }
 }
