f98514b0ee01253620667f34fd1b21c023b3d5be
haksong <gkrthdz@naver.com>
2022-01-10 22:21:33 +0900

    feat: 장바구니 메뉴 등록 후 list 조회 로직으로 변경

--- src/main/java/dev/toyproject/foodDelivery/order/application/OrderFacade.java
+++ src/main/java/dev/toyproject/foodDelivery/order/application/OrderFacade.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -16,14 +18,14 @@ public class OrderFacade {
 
     private final OrderFactory orderFactory;
     private final RedisCacheUtil redisCacheUtil;
+    private final OrderService orderService;
 
     /**
      * 장바구니 메뉴 등록
      *
      * @param command
      */
-    public void registerMenuBasket(OrderCommand.OrderBasketRequest command){
-        String hashKey = orderFactory.makeHashKey(command);
-        redisCacheUtil.setRedisCacheMenuBasket(command.getMemberToken(), RedisKeyFactory.MENU_SHOPPING_BASKET, hashKey, command);
+    public List<OrderCommand.OrderBasketRequest> registerMenuBasket(OrderCommand.OrderBasketRequest command){
+        return orderService.registerMenuBasket(command);
     }
 }
