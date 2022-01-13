b524db352620b4ee0afaa084c05badadbadf3186
haksong <gkrthdz@naver.com>
2022-01-13 21:37:18 +0900

    feat: Redis 메뉴 장바구니 Respose 수정

--- src/main/java/dev/toyproject/foodDelivery/order/domain/OrderInfo.java
+++ src/main/java/dev/toyproject/foodDelivery/order/domain/OrderInfo.java
@@ -1,55 +1,46 @@
 package dev.toyproject.foodDelivery.order.domain;
 
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
 
 import java.util.List;
 
 public class OrderInfo {
 
     @Getter
+    @Setter
+    @Builder
     @ToString
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class OrderBasketInfo{
         private String memberToken;
         private String shopToken;
-        private OrderBasketMenuInfo orderBasketMenuInfo;
-
-        public OrderBasketInfo(OrderCommand.OrderBasketRequest orderBasketCommand, OrderBasketMenuInfo orderBasketMenuInfo){
-            this.memberToken         = orderBasketCommand.getMemberToken();
-            this.shopToken           = orderBasketCommand.getShopToken();
-            this.orderBasketMenuInfo = orderBasketMenuInfo;
-        }
+        private OrderBasketMenuInfo orderBasketMenu;
     }
 
     @Getter
+    @Setter
+    @Builder
     @ToString
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class OrderBasketMenuInfo{
         private Long id;
         private String orderMenuName;
         private Long orderMenuCount;
         private Long orderMenuPrice;
-        private List<OrderBasketMenuOptionInfo> orderBasketMenuOptionInfoList;
-
-        public OrderBasketMenuInfo(OrderCommand.OrderBasketMenuRequest orderBasketMenuCommand, List<OrderBasketMenuOptionInfo> orderBasketMenuOptionInfoList){
-            this.id                            = orderBasketMenuCommand.getId();
-            this.orderMenuName                 = orderBasketMenuCommand.getOrderMenuName();
-            this.orderMenuCount                = orderBasketMenuCommand.getOrderMenuCount();
-            this.orderMenuPrice                = orderBasketMenuCommand.getOrderMenuPrice();
-            this.orderBasketMenuOptionInfoList = orderBasketMenuOptionInfoList;
-        }
+        private List<OrderBasketMenuOptionInfo> orderBasketMenuOptionList;
     }
 
     @Getter
+    @Setter
+    @Builder
     @ToString
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class OrderBasketMenuOptionInfo{
         private Long id;
         private String orderMenuOptionName;
         private Long orderMenuOptionPrice;
-
-        public OrderBasketMenuOptionInfo(OrderCommand.OrderBasketMenuOptionRequest orderBasketMenuOptionCommand){
-            this.id                         = orderBasketMenuOptionCommand.getId();
-            this.orderMenuOptionName        = orderBasketMenuOptionCommand.getOrderMenuOptionName();
-            this.orderMenuOptionPrice       = orderBasketMenuOptionCommand.getOrderMenuOptionPrice();
-        }
     }
 }
