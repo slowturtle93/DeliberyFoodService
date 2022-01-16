4869e93f42380d12c503b418c9208f667042fea1
haksong <gkrthdz@naver.com>
2022-01-16 22:48:11 +0900

    feat: 불필요한 코드 삭제

--- src/main/java/dev/toyproject/foodDelivery/order/infrastructure/OrderMenuSeriesFactoryImpl.java
+++ src/main/java/dev/toyproject/foodDelivery/order/infrastructure/OrderMenuSeriesFactoryImpl.java
@@ -3,21 +3,15 @@ package dev.toyproject.foodDelivery.order.infrastructure;
 import dev.toyproject.foodDelivery.order.domain.Order;
 import dev.toyproject.foodDelivery.order.domain.OrderCommand;
 import dev.toyproject.foodDelivery.order.domain.OrderMenuSeriesFactory;
-import dev.toyproject.foodDelivery.order.domain.OrderStore;
 import dev.toyproject.foodDelivery.order.domain.menu.OrderMenu;
-import dev.toyproject.foodDelivery.shop.domain.menu.MenuRead;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
 public class OrderMenuSeriesFactoryImpl implements OrderMenuSeriesFactory {
-
-    private final MenuRead menuRead;
-    private final OrderStore orderStore;
 
     /**
      * 주문 메뉴 및 주문 메뉴 옵션 등록
