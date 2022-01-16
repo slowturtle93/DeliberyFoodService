6fb0ffcc2d774a65f0858bb742068129a4b07e0c
haksong <gkrthdz@naver.com>
2022-01-16 22:04:48 +0900

    feat: 주문 등록 Command 추가

--- src/main/java/dev/toyproject/foodDelivery/order/domain/OrderCommand.java
+++ src/main/java/dev/toyproject/foodDelivery/order/domain/OrderCommand.java
@@ -1,10 +1,72 @@
 package dev.toyproject.foodDelivery.order.domain;
 
-import lombok.*;
+import dev.toyproject.foodDelivery.address.domain.AddressFragment;
+import dev.toyproject.foodDelivery.order.domain.menu.OrderMenu;
+import dev.toyproject.foodDelivery.order.domain.menu.OrderMenuOption;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
 
 public class OrderCommand {
+
+    @Getter
+    @Builder
+    @ToString
+    public static class RegisterOrder{
+        private String memberToken;
+        private String shopToken;
+        private String paymentMethod;
+        private Long totalAmount;
+        private AddressFragment addressFragment;
+        private List<OrderCommand.RegisterOrderMenu> orderMenuList;
+
+        public Order toEntity(){
+            return Order.builder()
+                    .memberToken(memberToken)
+                    .shopToken(shopToken)
+                    .paymentMethod(paymentMethod)
+                    .totalAmount(totalAmount)
+                    .addressFragment(addressFragment)
+                    .build();
+        }
+    }
+
+    @Getter
+    @Builder
+    @ToString
+    public static class RegisterOrderMenu{
+        private String orderMenuName;
+        private Long orderMenuCount;
+        private Long orderMenuPrice;
+
+        public OrderMenu toEntity(Order order){
+            return OrderMenu.builder()
+                    .order(order)
+                    .orderMenuName(orderMenuName)
+                    .orderMenuCount(orderMenuCount)
+                    .orderMenuPrice(orderMenuPrice)
+                    .build();
+        }
+    }
+
+    @Getter
+    @Builder
+    @ToString
+    public static class RegisterOrderMenuOption{
+        private String orderMenuOptionName;
+        private Long orderMenuOptionPrice;
+
+        public OrderMenuOption toEntity(OrderMenu orderMenu){
+            return OrderMenuOption.builder()
+                    .orderMenu(orderMenu)
+                    .orderMenuOptionName(orderMenuOptionName)
+                    .orderMenuOptionPrice(orderMenuOptionPrice)
+                    .build();
+        }
+    }
 
     @Getter
     @Setter
