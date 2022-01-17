a035e8cdba05b8e9e99383fabb497f784d8c8737
haksong <gkrthdz@naver.com>
2022-01-17 14:27:11 +0900

    feat: 주문 정보 등록 관련 SAVE 기능 구현

--- src/main/java/dev/toyproject/foodDelivery/order/infrastructure/OrderStoreImpl.java
+++ src/main/java/dev/toyproject/foodDelivery/order/infrastructure/OrderStoreImpl.java
@@ -2,6 +2,9 @@ package dev.toyproject.foodDelivery.order.infrastructure;
 
 import dev.toyproject.foodDelivery.order.domain.Order;
 import dev.toyproject.foodDelivery.order.domain.OrderStore;
+import dev.toyproject.foodDelivery.order.domain.menu.OrderMenu;
+import dev.toyproject.foodDelivery.order.domain.menu.OrderMenuOption;
+import dev.toyproject.foodDelivery.order.domain.menu.OrderMenuOptionGroup;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -12,9 +15,51 @@ import org.springframework.stereotype.Component;
 public class OrderStoreImpl implements OrderStore {
 
     private final OrderRepository orderRepository;
+    private final OrderMenuRepository orderMenuRepository;
+    private final OrderMenuOptionGroupRepository orderMenuOptionGroupRepository;
+    private final OrderMenuOptionRepository orderMenuOptionRepository;
 
+    /**
+     * 주문 정보 등록
+     *
+     * @param order
+     * @return
+     */
     @Override
     public Order store(Order order) {
         return orderRepository.save(order);
+    }
+
+    /**
+     * 주문 메뉴 정보 등록
+     *
+     * @param orderMenu
+     * @return
+     */
+    @Override
+    public OrderMenu store(OrderMenu orderMenu) {
+        return orderMenuRepository.save(orderMenu);
+    }
+
+    /**
+     * 주문 메뉴 옵션 그룹 정보 등록
+     *
+     * @param orderMenuOptionGroup
+     * @return
+     */
+    @Override
+    public OrderMenuOptionGroup store(OrderMenuOptionGroup orderMenuOptionGroup) {
+        return orderMenuOptionGroupRepository.save(orderMenuOptionGroup);
+    }
+
+    /**
+     * 주문 메뉴 옵션 정보 등록
+     *
+     * @param orderMenuOption
+     * @return
+     */
+    @Override
+    public OrderMenuOption store(OrderMenuOption orderMenuOption) {
+        return orderMenuOptionRepository.save(orderMenuOption);
     }
 }
