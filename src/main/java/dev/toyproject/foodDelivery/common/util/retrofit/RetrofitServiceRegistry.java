fdf86840a6c3598b3361cfe5c1046d6dcdcc7673
haksong <gkrthdz@naver.com>
2022-01-20 09:24:14 +0900

    feat: Retrofit Kakao Pay Bean 생성

--- src/main/java/dev/toyproject/foodDelivery/common/util/retrofit/RetrofitServiceRegistry.java
+++ src/main/java/dev/toyproject/foodDelivery/common/util/retrofit/RetrofitServiceRegistry.java
@@ -2,6 +2,7 @@ package dev.toyproject.foodDelivery.common.util.retrofit;
 
 import dev.toyproject.foodDelivery.address.infrastructure.RetrofitKakaoAddressApi;
 import dev.toyproject.foodDelivery.notification.sms.infrastructure.RetrofitNaverSensApi;
+import dev.toyproject.foodDelivery.order.infrastructure.payment.kakao.RetrofitKakaoPayApi;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -18,18 +19,29 @@ public class RetrofitServiceRegistry {
     // API Host 설정
     @Value("${spring.address-api.url}")
     private String kakaoAddressApi;
+
+    // kakaoPay Url
+    @Value("${payment.method.kakao.url}")
+    private String kakaoUPayUrl;
     
-    // Retrofit Bean 생성
+    // naver snes Retrofit Bean 생성
     @Bean
     public RetrofitNaverSensApi retrofitNaverSensApi(){
         var retrofit = RetrofitUtils.initRetrofit(NaverSmsApi);
         return retrofit.create(RetrofitNaverSensApi.class);
     }
 
-    // Retrofit Bean 생성
+    // kakao address Retrofit Bean 생성
     @Bean
     public RetrofitKakaoAddressApi retrofitKakaoAddressApi(){
         var retrofit = RetrofitUtils.initRetrofit(kakaoAddressApi);
         return retrofit.create(RetrofitKakaoAddressApi.class);
+    }
+
+    // kakao pay Retrofit Bean 생성
+    @Bean
+    public RetrofitKakaoPayApi retrofitKakaoApi() {
+        var retrofit = RetrofitUtils.initRetrofit(kakaoUPayUrl);
+        return retrofit.create(RetrofitKakaoPayApi.class);
     }
 }
