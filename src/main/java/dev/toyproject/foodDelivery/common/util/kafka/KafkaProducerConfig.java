91137d93a20824c40eeef951c55a7b4ff1b722e7
haksong <gkrthdz@naver.com>
2022-01-23 23:17:18 +0900

    feat: kafka ProducerConfig 파일 생성

---
+++ src/main/java/dev/toyproject/foodDelivery/common/util/kafka/KafkaProducerConfig.java
@@ -0,0 +1,35 @@
+package dev.toyproject.foodDelivery.common.util.kafka;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String BOOTSTRAP_SERVERS;
+
+    @Bean
+    public ProducerFactory<String, String> orderPaymentProducerFactory() {
+
+        Map<String,Object> configs = new HashMap<>();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configs);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaOrderPaymentTemplate() {
+        return new KafkaTemplate<>(orderPaymentProducerFactory());
+    }
+}
