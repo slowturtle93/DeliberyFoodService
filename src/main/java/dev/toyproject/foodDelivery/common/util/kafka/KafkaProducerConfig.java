d460c3437faccaa4cbaf5d544da35887358f3a36
haksong <gkrthdz@naver.com>
2022-01-24 13:26:27 +0900

    feat: kafka Producer Value Serializer JsonSerializer 변경 및 Map Value 변경

--- src/main/java/dev/toyproject/foodDelivery/common/util/kafka/KafkaProducerConfig.java
+++ src/main/java/dev/toyproject/foodDelivery/common/util/kafka/KafkaProducerConfig.java
@@ -1,7 +1,11 @@
 package dev.toyproject.foodDelivery.common.util.kafka;
 
+import dev.toyproject.foodDelivery.order.domain.OrderCommand;
+import dev.toyproject.foodDelivery.order.domain.OrderInfo;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +16,7 @@ import org.springframework.kafka.core.ProducerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @Configuration
 public class KafkaProducerConfig {
 
@@ -19,17 +24,17 @@ public class KafkaProducerConfig {
     private String BOOTSTRAP_SERVERS;
 
     @Bean
-    public ProducerFactory<String, String> orderPaymentProducerFactory() {
+    public ProducerFactory<String, OrderCommand.OrderPaymentConfirmRequest> orderPaymentProducerFactory() {
 
         Map<String,Object> configs = new HashMap<>();
         configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
         configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(configs);
     }
 
     @Bean
-    public KafkaTemplate<String, String> kafkaOrderPaymentTemplate() {
+    public KafkaTemplate<String, OrderCommand.OrderPaymentConfirmRequest> kafkaOrderPaymentTemplate() {
         return new KafkaTemplate<>(orderPaymentProducerFactory());
     }
 }
