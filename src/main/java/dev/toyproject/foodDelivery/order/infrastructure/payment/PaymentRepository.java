27fd152a19674c4b011a5c8d7ef9ef3db0ffeff0
haksong <gkrthdz@naver.com>
2022-01-26 09:40:17 +0900

    feat: 결제 Repository 추가

--- src/main/java/dev/toyproject/foodDelivery/order/infrastructure/payment/PaymentRepository.java
+++ src/main/java/dev/toyproject/foodDelivery/order/infrastructure/payment/PaymentRepository.java
@@ -3,5 +3,8 @@ package dev.toyproject.foodDelivery.order.infrastructure.payment;
 import dev.toyproject.foodDelivery.order.domain.payment.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Optional<Payment> findByPaymentToken(String paymentToken);
 }
