70055fefa70c0a238c6f441f5280006ef5885c30
haksong <gkrthdz@naver.com>
2022-01-26 22:20:33 +0900

    feat: 사장님 주문 요청 확인 FCM message 추가

--- src/main/java/dev/toyproject/foodDelivery/notification/fcm/infrastructrue/FcmNotificationInfo.java
+++ src/main/java/dev/toyproject/foodDelivery/notification/fcm/infrastructrue/FcmNotificationInfo.java
@@ -5,6 +5,10 @@ public class FcmNotificationInfo {
     private FcmNotificationInfo() {}
 
     // 주문 접수 FCM TITLE & MESSAGE
-    public static final String FCM_ORDER_INIT_TITLE = "[주문 접수 완료]";
+    public static final String FCM_ORDER_INIT_TITLE   = "[주문 접수 완료]";
     public static final String FCM_ORDER_INIT_MESSAGE = "고객님의 주문이 정상적으로 접수 되었습니다.";
+
+    // 사장님 주문 요청 확인 FCM TITLE & MESSAGE
+    public static final String FCM_OWNER_ORDER_CONFIRM_TITLE   = "[주문 확인 요청]";
+    public static final String FCM_OWNER_ORDER_CONFIRM_MESSAGE = "고객님이 음식을 주문하였습니다. 주문 내역을 확인해주세요.";
 }
