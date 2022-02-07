d3598c37f2a1a5a34dec52924953335fd3a33db7
haksong <gkrthdz@naver.com>
2022-02-07 09:36:25 +0900

    feat: Toss Pay 결제 취소 response 추가

--- src/main/java/dev/toyproject/foodDelivery/order/infrastructure/payment/toss/TossApiResponse.java
+++ src/main/java/dev/toyproject/foodDelivery/order/infrastructure/payment/toss/TossApiResponse.java
@@ -1,6 +1,5 @@
 package dev.toyproject.foodDelivery.order.infrastructure.payment.toss;
 
-import dev.toyproject.foodDelivery.order.domain.Order;
 import dev.toyproject.foodDelivery.order.domain.OrderCommand;
 import dev.toyproject.foodDelivery.order.domain.OrderInfo;
 import lombok.Builder;
@@ -27,5 +26,24 @@ public class TossApiResponse {
                     .redirectUrl(checkoutPage)
                     .build();
         }
+    }
+
+    @Getter
+    @Builder
+    @ToString
+    public static class refundResponse{
+        private final Integer code;
+        private final String refundNo;
+        private final Integer refundableAmount;
+        private final Integer discountedAmount;
+        private final Integer paidAmount;
+        private final Integer refundedAmount;
+        private final Integer refundedDiscountAmount;
+        private final Integer refundedPaidAmount;
+        private final String approvalTime;
+        private final String payToken;
+        private final String transactionId;
+        private final String msg;
+        private final String errorCode;
     }
 }
