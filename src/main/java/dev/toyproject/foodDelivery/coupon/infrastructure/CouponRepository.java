a809ba32b03574b25e98827713b2ccf953abffec
haksong <gkrthdz@naver.com>
2022-02-09 21:45:28 +0900

    feat: coupon 정보 조회 기능 추가

--- src/main/java/dev/toyproject/foodDelivery/coupon/infrastructure/CouponRepository.java
+++ src/main/java/dev/toyproject/foodDelivery/coupon/infrastructure/CouponRepository.java
@@ -3,5 +3,8 @@ package dev.toyproject.foodDelivery.coupon.infrastructure;
 import dev.toyproject.foodDelivery.coupon.domain.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
+    Optional<Coupon> findByCouponToken(String couponToken);
 }
