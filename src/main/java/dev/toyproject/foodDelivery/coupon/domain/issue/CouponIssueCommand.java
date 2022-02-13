ab99ad72abdbdf089149e49108a2582447f9d3b2
haksong <gkrthdz@naver.com>
2022-02-13 21:55:48 +0900

    feat: CouponIssueCommand 파일 생성 및 쿠폰 발행 Command 추가

---
+++ src/main/java/dev/toyproject/foodDelivery/coupon/domain/issue/CouponIssueCommand.java
@@ -0,0 +1,23 @@
+package dev.toyproject.foodDelivery.coupon.domain.issue;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+public class CouponIssueCommand {
+
+    @Getter
+    @Builder
+    @ToString
+    public static class Main{
+        private String couponToken;
+        private String memberToken;
+
+        public CouponIssue toEntity(){
+            return CouponIssue.builder()
+                    .couponToken(couponToken)
+                    .memberToken(memberToken)
+                    .build();
+        }
+    }
+}
