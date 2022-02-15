9a21eb0624798313fcf3840e5ee445bec22d66d3
haksong <gkrthdz@naver.com>
2022-02-15 15:17:16 +0900

    feat: CouponIssueInfo Convert 기능 추가

--- src/main/java/dev/toyproject/foodDelivery/coupon/infrastructure/CouponFactoryImpl.java
+++ src/main/java/dev/toyproject/foodDelivery/coupon/infrastructure/CouponFactoryImpl.java
@@ -3,6 +3,8 @@ package dev.toyproject.foodDelivery.coupon.infrastructure;
 import dev.toyproject.foodDelivery.coupon.domain.Coupon;
 import dev.toyproject.foodDelivery.coupon.domain.CouponFactory;
 import dev.toyproject.foodDelivery.coupon.domain.CouponInfo;
+import dev.toyproject.foodDelivery.coupon.domain.issue.CouponIssue;
+import dev.toyproject.foodDelivery.coupon.domain.issue.CouponIssueInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -15,10 +17,29 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class CouponFactoryImpl implements CouponFactory {
 
+    /**
+     * CouponInfo Response Convert
+     *
+     * @param couponList
+     * @return
+     */
     @Override
     public List<CouponInfo.Main> convertCouponInfoMain(List<Coupon> couponList) {
         return couponList.stream()
                 .map(CouponInfo.Main::new)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * CouponIssueInfo Response Convert
+     *
+     * @param couponIssueList
+     * @return
+     */
+    @Override
+    public List<CouponIssueInfo.Main> convertCouponIssueInfoMain(List<CouponIssue> couponIssueList) {
+        return couponIssueList.stream()
+                .map(CouponIssueInfo.Main::new)
                 .collect(Collectors.toList());
     }
 }
