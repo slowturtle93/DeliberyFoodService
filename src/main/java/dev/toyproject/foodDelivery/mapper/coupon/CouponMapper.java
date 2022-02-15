de41504d96e521b847c99b09279d88ef09fa1c3a
haksong <gkrthdz@naver.com>
2022-02-15 15:18:56 +0900

    feat: 사용 기간 지난 쿠폰 상태 변경 및 사용 가능한 쿠폰 list 조회 기능 추가

--- src/main/java/dev/toyproject/foodDelivery/mapper/coupon/CouponMapper.java
+++ src/main/java/dev/toyproject/foodDelivery/mapper/coupon/CouponMapper.java
@@ -1,6 +1,8 @@
 package dev.toyproject.foodDelivery.mapper.coupon;
 
 import dev.toyproject.foodDelivery.coupon.domain.Coupon;
+import dev.toyproject.foodDelivery.coupon.domain.issue.CouponIssue;
+import dev.toyproject.foodDelivery.coupon.domain.issue.CouponIssueCommand;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -8,4 +10,8 @@ import java.util.List;
 @Mapper
 public interface CouponMapper {
     public List<Coupon> findCouponEnable();
+
+    public void updateCouponIssueStatusEnd(CouponIssueCommand.RetrieveCouponIssue command);
+
+    public List<CouponIssue> findAllCouponIssueEnable(CouponIssueCommand.RetrieveCouponIssue command);
 }
