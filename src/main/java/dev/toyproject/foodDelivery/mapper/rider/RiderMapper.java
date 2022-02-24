26b045e6d3b458ba7c7af69207b50b7e8f1b4eb4
haksong <gkrthdz@naver.com>
2022-02-24 20:20:50 +0900

    feat: feat: 라이더 1건 배달 접수 주문 정보 조회 기능 추가

--- src/main/java/dev/toyproject/foodDelivery/mapper/rider/RiderMapper.java
+++ src/main/java/dev/toyproject/foodDelivery/mapper/rider/RiderMapper.java
@@ -9,4 +9,6 @@ import java.util.List;
 @Mapper
 public interface RiderMapper {
     List<RiderInfo.AvailableOrders> getOrdersAvailableDelivery(RiderCommand.RiderCurrentLocation command);
+
+    RiderInfo.AvailableOrders getDeliveryOrderPickInfo(String orderToken);
 }
